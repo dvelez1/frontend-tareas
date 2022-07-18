c59c997eb0c8b7c28d4f99e85582ad1d61e8db3f
Dennis Velez <36821370+dvelez1@users.noreply.github.com>
2022-07-18 18:11:17 -0400

    Add icons and Install Bootstap

--- src/components/Tarea.vue
+++ src/components/Tarea.vue
@@ -9,30 +9,61 @@
             <div class="card-body">
               <div class="input-group">
                 <!-- Bing property data.tarea to input textbox -->
-                <input type="text" v-model="tarea"
-                  class="text form-control form-control-lg" placeholder="Agregar Tarea"/>
+                <input
+                  type="text"
+                  v-model="tarea"
+                  class="text form-control form-control-lg"
+                  placeholder="Agregar Tarea"
+                />
                 <div class="input-group-append">
-                  <button v-on:click="agregarTarea()" 
-                      class="btn-success btn-lg">Agregar</button>
+                  <button
+                    v-on:click="agregarTarea()"
+                    class="btn-success btn-lg"
+                  >
+                    Agregar
+                  </button>
                 </div>
               </div>
-              <br>
+              <br />
+              <div class="text-center">
+                <!-- Spinner will triger when variable loading is true / otherwise will be hidden -->
+                <div
+                  v-if="loading"
+                  class="spinner-grow text-success"
+                  role="status"
+                >
+                  <span class="sr-only">Loading...</span>
+                </div>
+              </div>
               <!--listTareas -> Property on Data Layer  -->
-              <h5 v-if="listTareas.length == 0" >No hay tareas para realizar</h5>
+              <h5 v-if="listTareas.length == 0">No hay tareas para realizar</h5>
 
               <ul class="list-group">
                 <!-- Data Layer ->tarea: representa mi propiedad tarea / listTareas = Propiedad tipo Lista  -->
-                <li v-for="(tarea, index) of listTareas" :key="index"
-                    class="list-group-item d-flex justify-content-between">
-                    <!-- See below if statement, when tarea.estado equal to true, apply color (class: text-success) -->
-                  <span class="cursor" v-bind:class="{'text-success' : tarea.estado}"
-                    v-on:click="editarTarea(tarea, index)">
+                <li
+                  v-for="(tarea, index) of listTareas"
+                  :key="index"
+                  class="list-group-item d-flex justify-content-between"
+                >
+                  <!-- See below if statement, when tarea.estado equal to true, apply color (class: text-success) -->
+                  <span
+                    class="cursor"
+                    v-bind:class="{ 'text-success': tarea.estado }"
+                    v-on:click="editarTarea(tarea, tarea.id)"
+                  >
                     <!-- See below if to exchange icons based on tarea.estado -->
-                    <i v-bind:class="[tarea.estado ? 'fas fa-check-circle' : 'far fa-circle']"></i>
+                    <i
+                      v-bind:class="[
+                        tarea.estado ? 'fas fa-check-circle' : 'far fa-circle',
+                      ]"
+                    ></i>
                   </span>
                   <!-- Constante objeto tarea con propiedades (Nombe ->Que a su ves es la prop tarea & propiedad estado) -->
                   {{ tarea.nombre }}
-                  <span class="text-danger cursor" v-on:click="eliminarTarea(index)">
+                  <span
+                    class="text-danger cursor"
+                    v-on:click="eliminarTarea(tarea.id)"
+                  >
                     <i class="fas fa-trash-alt"></i>
                   </span>
                 </li>
@@ -46,29 +77,86 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Tarea",
   data() {
     return {
       tarea: "",
       listTareas: [],
+      loading: false,
     };
   },
   methods: {
     agregarTarea() {
       const tarea = {
         nombre: this.tarea,
-        estado: false,
       };
-      this.listTareas.push(tarea);
+      // this.listTareas.push(tarea);
+      this.loading = true;
+      axios
+        .post("https://localhost:44384/api/Tarea/", tarea)
+        .then((response) => {
+          console.log(response);
+          this.loading = false;
+          this.obtenerTareas();
+        })
+        .catch((error) => {
+          console.error(error);
+          this.loading = false;
+        });
+
       this.tarea = "";
     },
-    eliminarTarea(index) {
-      this.listTareas.splice(index, 1);
+    eliminarTarea(id) {
+      /* this.listTareas.splice(index, 1); */
+      this.loading = true;
+      axios
+        .delete("https://localhost:44384/api/Tarea/" + id)
+        .then((response) => {
+          console.log(response);
+          this.loading = false;
+          this.obtenerTareas();
+        })
+        .catch((error) => {
+          console.log(error);
+          this.loading = false;
+        });
     },
-    editarTarea(tarea, index){
-      this.listTareas[index].estado = !tarea.estado;
-    }
+    editarTarea(tarea, id) {
+      // this.listTareas[index].estado = !tarea.estado;
+      this.loading = true;
+      axios
+        .put("https://localhost:44384/api/Tarea/" + id, tarea)
+        .then((response) => {
+          console.log(response);
+          this.loading = false;
+          this.obtenerTareas();
+        })
+        .catch((error) => {
+          this.loading = false;
+          console.log(error);
+        });    
+          
+    },
+    obtenerTareas() {
+      this.loading = true;
+      axios
+        .get("https://localhost:44384/api/Tarea")
+        .then((response) => {
+          console.log(response);
+          this.listTareas = response.data;
+          this.loading = false;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.loading = false;
+        });
+    },
+  },
+  created: function () {
+    this.obtenerTareas();
   },
 };
 </script>
